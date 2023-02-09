8cf0fe331dd3c8fa28e0fda79c8b25f295cbf2d8
Jason1111TW <jjason1111tw@gmail.com>
2023-02-09 13:48:42 +0800

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+// 讀取JSON資料;
 function GetJSON(stock_num){
   var requestURL = "/static/data/" + stock_num + ".json";
   var request = new XMLHttpRequest();
@@ -12,64 +13,161 @@ function GetJSON(stock_num){
   return request
 }
 
-// 歷年股利; DIVIDEND
-
-function Echarts_DIVIDEND(){
-
-}
-
-(function () {
-  console.log("Check~")
+// 歷年股利;
+function Echarts_Dividend(data){
   var myChart = echarts.init(document.querySelector(".Dividend .chart"));
   var option = {
-
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{
+      type: 'category',
+      data: data["Time"]
+    },
+    yAxis:{
+      type: 'value'
+    },
+    series:[{
+      name: '現金股利',
+      type: 'bar',
+      data: data["現金股利"]
+    },
+    {
+      name: '股票股利',
+      type: 'bar',
+      data: data["股票股利"]
+    }]
   }
 
-
   myChart.setOption(option);
-  // 圖表隨螢幕調整大小;
   window.addEventListener('resize', function () {
     myChart.resize();
   })
-})();
+}
 
-// 股東結構; Sharehold
-(function () {
+// 股東結構;
+function Echarts_Sharehold(data){
+  var myChart = echarts.init(document.querySelector(".Sharehold .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
-
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
 // 股票體質分析;
-(function () {
+function Echarts_Analysis(data){
+  var myChart = echarts.init(document.querySelector(".Analysis .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
-
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
 // K線圖;
-(function () {
+function Echarts_KLine(data){
+  var myChart = echarts.init(document.querySelector(".K-line .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
+// 財務比率表;
+function Echarts_FinDetail(data){
+  var myChart = echarts.init(document.querySelector(".FinDetail .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-// 財務比率表; FinDetail
-(function () {
-
-})();
-
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
 // 總表;
-(function () {
+function Echarts_Total(data){
+  var myChart = echarts.init(document.querySelector(".Total .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
-// 年資料; ProfitIndicator
-(function () {
+// 年資料;
+function Echarts_Year(data){
+  var myChart = echarts.init(document.querySelector(".Year .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
-// 季資料; ProfitIndicator
-(function () {
+// 季資料;
+function Echarts_Season(data){
+  var myChart = echarts.init(document.querySelector(".Season .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
 
-})();
-
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
 
