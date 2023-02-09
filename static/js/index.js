227ef240c62a14f9a622422a2ab3fdac19f6f164
Jason1111TW <jjason1111tw@gmail.com>
2023-02-09 16:49:36 +0800

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -4,11 +4,14 @@ function GetJSON(stock_num){
   var request = new XMLHttpRequest();
   request.open('GET', requestURL, true);
   request.send();
+  var test = [];
   request.onload = function () {
     var data = JSON.parse(this.responseText);
+    test.push(data)
     console.log("@_@",this)
     console.log(data);
   }
+  console.log(test)
   console.log("CHECK",request)
   console.log("@",JSON.parse(request.responseText))
 
