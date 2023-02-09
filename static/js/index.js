0e2afbf8b9bd636e1f57f60956f95599a5e62fc5
Jason1111TW <jjason1111tw@gmail.com>
2023-02-09 18:35:50 +0800

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,90 +1,19 @@
-// 讀取JSON資料;
-(function () {
-  var stock_num = "3010";
-  
+/* ------------------------------------------------------------------------------
+ *  # Echarts
+ *  @author: Chien-Yu, Lu
+ *  Created on 2023/02/09
+ * ---------------------------------------------------------------------------- */
+
+var Echarts_Stock = function(stock_num) {
+  // 讀取JSON資料;
   var requestURL = "/static/data/" + stock_num + ".json";
   var request = new XMLHttpRequest();
   request.open('GET', requestURL, true);
   request.send();
-  var TEMP = [];
+  var Data = [];
   request.onload = function () {
-    var data = JSON.parse(this.responseText);
-    console.log(data)
-    TEMP.push(data)
+    var temp = JSON.parse(this.responseText);
+    Data.push(temp);
   }
-  console.log(TEMP)
-
-  (function () {
-      // 1.实例化对象
-  var myChart = echarts.init(document.querySelector(".Dividend .chart"));
-  // 2.指定配置项和数据
-  var option = {
-    color: ['#2f89cf'],
-    // 提示框组件
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: { // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-      }
-    },
-    // 修改图表位置大小
-    grid: {
-      left: '0%',
-      top: '10px',
-      right: '0%',
-      bottom: '4%',
-      containLabel: true
-    },
-    // x轴相关配置
-    xAxis: [{
-      type: 'category',
-      data: ["旅游行业", "教育培训", "游戏行业", "医疗行业", "电商行业", "社交行业", "金融行业"],
-      axisTick: {
-        alignWithLabel: true
-      },
-      // 修改刻度标签，相关样式
-      axisLabel: {
-        color: "rgba(255,255,255,0.8)",
-        fontSize: 10
-      },
-      // x轴样式不显示
-      axisLine: {
-        show: false
-      }
-    }],
-    yAxis: [{
-      type: 'value',
-      axisLabel: {
-        color: "rgba(255,255,255,0.6)",
-        fontSize: 12
-      },
-      axisLine: {
-        lineStyle: {
-          color: "rgba(255,255,255,0.6)",
-          width: 2
-        }
-      },
-      splitLine: {
-        lineStyle: {
-          color: "rgba(255,255,255,0.1)"
-        }
-      }
-    }],
-    series: [{
-      name: '直接访问',
-      type: 'bar',
-      barWidth: '35%',
-      data: [200, 300, 300, 900, 1500, 1200, 600],
-      itemStyle: {
-
-        barBorderRadius: 5
-      }
-    }]
-  };
-  myChart.setOption(option);
-  window.addEventListener('resize', function () {
-    myChart.resize();
-  })
-  })();
-})();
-
+  console.log(Data)
+}
