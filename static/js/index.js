785908127df3cd1c110c916f749ce9729dda72bd
Jason1111TW <jjason1111tw@gmail.com>
2023-02-09 09:35:39 +0800

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,18 @@
-(function (JSONdata) {
+(function () {
   var requestURL = "/static/data/3010.json"
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
   request.send();
-  request.onload = function(JSONdata) {
-    console.log("TEST");
-    var JSONdata = request.response;
-    //console.log(JSONdata);  
-    
+  request.onload = function() {
+    const JSONdata = request.response;
+    console.log("CHECK1~",JSONdata);
   }
-  console.log(JSONdata)
 })();
 
 // 歷年股利;
 (function () {
   var myChart = echarts.init(document.querySelector(".Dividend .chart"));
-  console.log("999",JSONdata)
   var option = {
 
   }
