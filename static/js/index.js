a49fc99f17a41050618d1badfc5c06e61f97a314
Jason1111TW <jjason1111tw@gmail.com>
2023-02-09 19:07:55 +0800

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -48,3 +48,129 @@ var Echarts_Dividend = function(data){
     myChart.resize();
   })
 }
+
+// 股東結構;
+var Echarts_Sharehold =  function(data){
+  var myChart = echarts.init(document.querySelector(".Sharehold .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// 股票體質分析;
+var Echarts_Analysis = function(data){
+  var myChart = echarts.init(document.querySelector(".Analysis .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// K線圖;
+var Echarts_KLine = function(data){
+  var myChart = echarts.init(document.querySelector(".K-line .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// 財務比率表;
+var Echarts_FinDetail = function(data){
+  var myChart = echarts.init(document.querySelector(".FinDetail .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// 總表;
+var Echarts_Total = function(data){
+  var myChart = echarts.init(document.querySelector(".Total .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// 年資料;
+var Echarts_Year= function(data){
+  var myChart = echarts.init(document.querySelector(".Year .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
+
+// 季資料;
+var Echarts_Season = function(data){
+  var myChart = echarts.init(document.querySelector(".Season .chart"));
+  var option = {
+    tooltip:{},
+    toolbox:{},
+    grid:{},
+    xAxis:{},
+    yAxis:{},
+    series:[{}]
+  }
+
+  myChart.setOption(option);
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  })
+}
